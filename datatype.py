12864af87d6ffbcf646507ff65b40c47ca9c7979
TabithaClitus <tabithaclitus@gmail.com>
2025-08-24 19:34:47 +0530

    initial commit

---
+++ datatype.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number:"))
+print(a,type(a))
+
+f=float(input("Enter a decimal number:"))
+print(f,type(f))
+
+b=input("Enter a string:")
+print(b,type(b))
+
+c=input("Enter a character: ")[0]
+print(c,type(c))
